cf12298f235e742cdba6d3253b7b01b1e6dd2892
ahin0812 <ahin2801@gmail.com>
2024-10-04 20:46:25 +0900

    Add files via upload

---
+++ 20241004(2).cpp
@@ -0,0 +1,32 @@
+/*
+#include<stdio.h>
+int main()
+{
+	int w, h;
+	double s;
+	
+	scanf("%d %d", &w, &h);
+	
+	s = 1.0*w*h/2;
+	
+	printf("%lf", s);
+	
+	return 0;
+}
+*/
+
+#include<stdio.h>
+int main()
+{
+	
+	int c;
+	double f;
+	
+	scanf("%d", &c);
+	f= (1.0*c*9/5)+32;
+	
+	printf("%lf", f);
+	
+	return 0;
+}
+ 
