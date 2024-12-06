20846b74abaea548aaa5bb2db413903df7545881
ahin0812 <ahin2801@gmail.com>
2024-12-06 20:09:36 +0900

    Add files via upload

---
+++ 20241206(3).cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int page[10]={123,57,67,86};
+	for(int i=0;i<10;++i)
+	{
+		printf("%d ",page[i]);
+	}
+	return 0;
+}
---
+++ 20241206(4).cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	char c;int i;long l;float f;double d;
+	printf("%d %d %d %d %d\n",sizeof(c),sizeof(i),sizeof(l),sizeof(f),sizeof(d));
+	return 0;
+}
