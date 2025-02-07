2ef33c3bfea09f651bb17970109ce10b4c098d3f
ahin0812 <ahin2801@gmail.com>
2025-02-07 19:51:22 +0900

    Add files via upload

---
+++ 20250207(3).cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	char str1[15],str2[15];
+	
+	scanf("%s",str1);
+	printf("str1=%s\n",str1);
+	scanf("%s%s",str1,str2);
+	printf("str1=%s\n",str1);
+	printf("str2=%s\n",str2);	
+	return 0;
+}
