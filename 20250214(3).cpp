310f9f5eca8b66f35b5ba6339f16d89e534a76ad
ahin0812 <ahin2801@gmail.com>
2025-02-14 20:24:28 +0900

    Add files via upload

---
+++ 20250214(3).cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+int main()
+{
+	int x,y;
+	COORD pos;
+	do
+	{
+		pos.X=0;
+		pos.Y=0;
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),pos);
+		printf("커서의 위치를 입력하세요:");
+		scanf("%d %d",&pos.X,&pos.Y);
+		system("cls");
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),pos);
+		printf("+",pos.X,&pos.Y);
+		getch();
+	}while(pos.X<=80 &&pos.Y<=24);
+	return 0;
+}
